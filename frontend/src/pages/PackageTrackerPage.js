5b83accba089d4097e56403b23e82849ef60bf78
teebee2187 <travisw.bakri@gmail.com>
2026-04-19 22:00:12 -0500

    Create PackageTrackerPage.js

---
+++ frontend/src/pages/PackageTrackerPage.js
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+function PackageTrackerPage() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "80px" }}>
+    <h1> Track Your Packages </h1>
+  );
+}
+export default PackageTrackerPage;
